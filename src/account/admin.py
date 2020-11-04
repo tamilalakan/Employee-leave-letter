682384e85b046253bc39d0e6e4b0b0a82641037e
Tamilalakan <43065774+tamilalakan@users.noreply.github.com>
2020-11-04 12:27:13 +0530

    Update admin.py

--- src/account/admin.py
+++ src/account/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from account.models import Employee
 # Register your models here.
 admin.site.register(Employee)
+from django.contrib.admin.models import LogEntry
+LogEntry.objects.all().delete()
